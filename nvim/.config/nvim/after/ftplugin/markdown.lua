5867489490449b35c4e2e986f82b0655ba28284a
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-10-04 21:24:11 -0400

    update to markdown filetype settings

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,9 +1,11 @@
--- vim.cmd [[
--- " Only do this when not done yet for this buffer
--- if exists("b:did_ftplugin")
---   finish
--- endif
--- let b:did_ftplugin = 1
--- ]]
-
 vim.opt.wrap = true            -- Enable line wrap
+vim.opt.cursorline = false       -- Highlight the current line
+vim.opt.inccommand = "split"    -- Preview substitutions live, as you type!
+vim.opt.autoindent = true
+vim.opt.autoread = true
+vim.opt.softtabstop = 4
+vim.opt.numberwidth = 4
+vim.opt.cursorcolumn = false
+vim.opt.colorcolumn = "0"
+vim.opt.showmode = true        -- Don't show mode in command line
+
