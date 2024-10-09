93f9ff1aca42597448a4e9485d1efc71ccd372cf
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-10-09 02:57:48 -0400

    add note script

---
+++ bin/bin/note.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+note="$HOME/repos/notes/inbox/$(date +%Y-%m-%d).md"
+
+if [ ! -f $note ]; then
+  echo "# $(date +%A\ %B\ %d\ %Y)" > $note
+fi
+
+nvim -c "norm Go" \
+  -c "norm Go## $(date +%r)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $note
