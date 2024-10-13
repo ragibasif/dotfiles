7cad65bf3b29ed39ae8d3b97efd35f42c0f39d97
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-10-13 14:08:27 -0400

    add new nvim config file

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- This is the default leader key
+vim.g.mapleader = '\'
+vim.g.maplocalleader = '\'
+
+-- Only make true if a Nerd Font is installed
+vim.g.have_nerd_font = true
+
+vim: ts=2 sts=2 sw=2 et
