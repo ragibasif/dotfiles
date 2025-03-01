b37df88f7f6c5943fdd9c4013ca534eb1fc7a348
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-01 01:21:18 -0500

    {{UPDATE}} Sat Mar  1 01:21:18 2025

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,7 @@
 return {
     'lewis6991/gitsigns.nvim',
     config = function()
-        require('gitsigns').setup{
-        }
+        require('gitsigns').setup{}
     end,
 }
 
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "cpp" } },
+    opts = { ensure_installed = { "cpp", "c", "lua", "python", "javascript, typescript, html, css, json, cmake, make, markdown, bash, asm, nasm"} },
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
