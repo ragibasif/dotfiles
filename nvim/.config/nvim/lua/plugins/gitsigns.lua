622588826bae8e896079ce2da28c23aa0d6b14b8
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-02 12:50:08 -0500

    {{UPDATE}} Sun Mar  2 12:50:08 2025

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -23,7 +23,7 @@ return {
 			current_line_blame_opts = {
 				virt_text = true, -- Show blame in virtual text
 				virt_text_pos = "eol", -- Position of virtual text (end of line)
-				delay = 100, -- Delay before showing blame
+				delay = 1000, -- Delay before showing blame
 				ignore_whitespace = false, -- Don't ignore whitespace changes
 			},
 			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>", -- Blame format
