fbe0eea064395a386d2552d187eb53108b9cc2ef
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-02 13:04:56 -0500

    {{UPDATE}} Sun Mar  2 13:04:56 2025

---
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+-- Python-specific settings
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true -- Use spaces instead of tabs
+vim.bo.textwidth = 88 -- PEP 8 line length
