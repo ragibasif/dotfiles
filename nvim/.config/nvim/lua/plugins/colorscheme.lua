f20dce747930e47c3f075325dbacdf6b4eb7aba9
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-02 12:52:00 -0500

    {{UPDATE}} Sun Mar  2 12:52:00 2025

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,3 @@
--- return {
--- 	-- the colorscheme should be available when starting Neovim
--- 	"folke/tokyonight.nvim",
--- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
--- 	priority = 1000, -- make sure to load this before all the other start plugins
--- 	config = function()
--- 		-- load the colorscheme here
--- 		vim.cmd([[colorscheme tokyonight]])
--- 	end,
--- 	-- colorscheme list
--- }
-
--- plugins/colorschemes.lua
 return {
 	{
 		"folke/tokyonight.nvim", -- TokyoNight colorscheme
@@ -25,36 +12,11 @@ return {
 		end,
 	},
 	{
-		"navarasu/onedark.nvim", -- OneDark colorscheme
-		lazy = true, -- Lazy-load this plugin
-		config = function()
-			require("onedark").setup({
-				style = "dark", -- Available styles: dark, darker, cool, deep, warm, warmer, light
-			})
-		end,
-	},
-	{
 		"ellisonleao/gruvbox.nvim", -- Gruvbox colorscheme
 		lazy = true, -- Lazy-load this plugin
 		config = function()
 			require("gruvbox").setup({
 				contrast = "hard", -- Available options: soft, medium, hard
-			})
-		end,
-	},
-	{
-		"sainnhe/sonokai", -- Sonokai colorscheme
-		lazy = true, -- Lazy-load this plugin
-		config = function()
-			vim.g.sonokai_style = "andromeda" -- Available styles: default, atlantis, andromeda, shusia, maia, espresso
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim", -- Kanagawa colorscheme
-		lazy = true, -- Lazy-load this plugin
-		config = function()
-			require("kanagawa").setup({
-				theme = "wave", -- Available themes: wave, dragon, lotus
 			})
 		end,
 	},
