788526af3f3a836aff6d35215aecda0ab017bafb
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-06 15:19:24 -0500

    disable  smooth scroll in nvim

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -15,17 +15,16 @@ local footer = [[
                                       n e o v i m   ]]
 
 return {
-        "folke/snacks.nvim",
-        priority = 1000,
-        lazy = false,
-        init = function()
-        end,
-        opts = {
-            dashboard = {
-                width = 46,
-                preset = {
-                    header = header,
-                    keys = {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	init = function() end,
+	opts = {
+		dashboard = {
+			width = 46,
+			preset = {
+				header = header,
+				keys = {
                         -- stylua: ignore start
                         { icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
                         { icon = " ", key = "n", desc = "New File", action = ":enew | startinsert" },
@@ -36,45 +35,44 @@ return {
                         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
                         { icon = "󱌢 ", key = "m", desc = "Mason", action = ":Mason" },
                         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                        -- stylua: ignore end
-                    },
-                },
-                sections = {
-                    { section = "header", gap = 0, padding = 0 },
-                    { section = "keys", gap = 1, padding = 1 },
-                    {
-                        text = {
-                            {
-                                footer,
-                                hl = "SnacksDashboardHeader",
-                            },
-                        },
-                        gap = 0,
-                        padding = 1,
-                    },
-                    { section = "startup", gap = 0, padding = 0 },
-                },
-            },
-            styles = {
-                notification = { border = "single" },
-                notification_history = { border = "single" },
-            },
-            git = { enabled = true },
-            indent = { enabled = true },
-            input = { enabled = true },
-            notifier = {
-                enabled = true,
-                icons = {
-                    error = "󰯸 ",
-                    warn = "󰰮 ",
-                    info = "󰰄 ",
-                    debug = "󰯵 ",
-                    trace = "󰰥 ",
-                },
-            },
-            scroll = { enabled = true },
-            statuscolumn = {},
-        },
-        keys = {
-        },
-    }
+					-- stylua: ignore end
+				},
+			},
+			sections = {
+				{ section = "header", gap = 0, padding = 0 },
+				{ section = "keys", gap = 1, padding = 1 },
+				{
+					text = {
+						{
+							footer,
+							hl = "SnacksDashboardHeader",
+						},
+					},
+					gap = 0,
+					padding = 1,
+				},
+				{ section = "startup", gap = 0, padding = 0 },
+			},
+		},
+		styles = {
+			notification = { border = "single" },
+			notification_history = { border = "single" },
+		},
+		git = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = {
+			enabled = true,
+			icons = {
+				error = "󰯸 ",
+				warn = "󰰮 ",
+				info = "󰰄 ",
+				debug = "󰯵 ",
+				trace = "󰰥 ",
+			},
+		},
+		scroll = { enabled = false }, -- smooth scroll but its actuall really annoying to me so keep it disabled
+		statuscolumn = {},
+	},
+	keys = {},
+}
