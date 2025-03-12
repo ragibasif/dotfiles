eb87725533c6b81df519663c27e77ab65274826d
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-12 11:17:35 -0400

    update: python.lua in nvim ftplugin/

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -3,4 +3,6 @@ vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.expandtab = true -- Use spaces instead of tabs
-vim.bo.textwidth = 88 -- PEP 8 line length
+vim.bo.textwidth = 79
+vim.opt.foldenable = false
+vim.g.python_highlight_all = 1
