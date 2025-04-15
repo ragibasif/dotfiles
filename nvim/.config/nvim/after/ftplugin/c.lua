43467043760045a10341cb8612eda761ed86d7be
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-04-14 21:43:21 -0400

    update nvim

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -2,5 +2,5 @@
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
-vim.bo.expandtab = false -- Use tabs instead of spaces
+vim.bo.expandtab = true -- Use spaces instead of tabs
 vim.bo.cindent = true -- Enable C-style indentation
