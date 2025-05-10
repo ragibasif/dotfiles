54524c5a758da3ee15326e819655034298af7f93
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-05-10 19:46:38 -0400

    clean up scripts

--- bin/bin/git_reset.sh
+++ bin/bin/git_reset.sh
@@ -6,13 +6,10 @@ set -vx
 # clear history of remote git repo
 # https://stackoverflow.com/questions/9683279/make-the-current-commit-the-only-initial-commit-in-a-git-repository
 
-
-master=master
-
 git checkout --orphan newBranch
-git add -A  # Add all files and commit them
+git add -A # Add all files and commit them
 git commit -m "reset"
-git branch -D $master  # Deletes the master branch
-git branch -m $master  # Rename the current branch to master
-git push -f --set-upstream origin $master # Force push master branch to github
-git gc --aggressive --prune=all # remove the old files
+git branch -D master                     # Deletes the master branch
+git branch -m master                     # Rename the current branch to master
+git push -f --set-upstream origin master # Force push master branch to github
+git gc --aggressive --prune=all          # remove the old files
--- bin/bin/git_update.sh
+++ bin/bin/git_update.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Backup/update current directory to github
-#
-
-# set -x
 set -vx
 
-master=master
-
-git add -A  # Add all files and commit them
-git commit -m "{{AUTOMATED UPDATE}} $(date +%c)"
-git push origin $master # push master branch to github
+git add -A # Add all files and commit them
+git commit -m "backup: automated update $(date +%c)"
+git push origin master # push master branch to github
